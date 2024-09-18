098d22608266f02a7c5236a174589069a349a255
ZigCat <zigcat517@gmail.com>
2024-09-18 18:19:10 +0500

    initial commit

---
+++ src/test/java/com/github/zigcat/solva_assignment/SolvaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zigcat.solva_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolvaAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
